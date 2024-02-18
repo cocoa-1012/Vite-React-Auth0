6f5e3b2ae3d4edad07c2a989a5b17e09f94731da
cocoa-1012 <cocoa.pandaok@gmail.com>
2024-02-18 08:25:06 +0700

    code clean

--- frontend/src/app/state/slices/gameSetup/types.ts
+++ frontend/src/app/state/slices/gameSetup/types.ts
@@ -1,30 +1,30 @@
-export interface InitialStateTypes {
-  title: string;
-  description: string;
-  date: string;
-  time: string;
-  place: string;
-  bgaProfile: string;
-  boardgamegreek: string;
+export type InitialStateTypes = {
+  title: string,
+  description: string,
+  date: string,
+  time: string,
+  place: string,
+  bgaProfile: string,
+  boardgamegreek: string,
   collectionGames: {
-    id: number;
-    game: string;
-    release: number;
-    playerCount: string;
-    playTime: number;
-    weight: number;
-    rating: number;
-  }[];
+    id: number,
+    game: string,
+    release: number,
+    playerCount: string,
+    playTime: number,
+    weight: number,
+    rating: number,
+  }[],
   nightGames: {
-    id: number;
-    game: string;
-    release: number;
-    playerCount: string;
-    playTime: number;
-    weight: number;
-    rating: number;
-  }[];
-  friends: string[];
+    id: number,
+    game: string,
+    release: number,
+    playerCount: string,
+    playTime: number,
+    weight: number,
+    rating: number,
+  }[],
+  friends: string[],
 }
 
-export type GameSetupState = InitialStateTypes;
+export type GameSetupState = InitialStateTypes
